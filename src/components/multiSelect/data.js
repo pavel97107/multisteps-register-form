fe9e6f052cb518603380da6be91f085c5e618870
pavel97107 <pavel97107@gmail.com>
2020-12-09 23:43:51 +0300

    production_v1

--- src/components/multiSelect/data.js
+++
@@ -1,5 +0,0 @@
-export const list = [
-  { title: "Box", id: 1, isSelected: false },
-  { title: "Design", id: 2, isSelected: false },
-  { title: "Code", id: 3, isSelected: false },
-];
--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as PersonalData} from './personalData'
-export {default as ConfidentialData} from './ConfidentialData'
-export {default as AboutMySelf} from './AboutMySelf'
-export {default as Finish} from './Finish'
